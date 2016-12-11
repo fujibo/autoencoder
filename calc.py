070f56965faccf513d4bd2f5dbaa5140dbde75a6
fujibo <kt.tsubo@gmail.com>
2016-12-11 12:43:03 +0900

    データを作るところを微修正

--- calc.py
+++ calc.py
@@ -6,23 +6,19 @@ def makeData(operators):
     outputdata = []
     for i in range(1, 10):
         for j in range(1, 10):
+            datumI = [i, j]
             datumO = []
             for op in operators:
                 if op == '+':
-                    datumI = "{:02d}+{:02d}".format(i, j)
                     datumO.append(i + j)
                 elif op == '-':
-                    datumI = "{:02d}-{:02d}".format(i, j)
                     datumO.append(i - j)
                 elif op == '*':
-                    datumI = "{:02d}*{:02d}".format(i, j)
                     datumO.append(i * j)
                 elif op == '/':
-                    datumI = "{:02d}/{:02d}".format(i, j)
                     datumO.append(i / j)
 
-                inputdata.append([int(datumI[0:2]), int(datumI[3:])])
-                # inputdata.append([int(datumI[0]), int(datumI[2])])
+                inputdata.append(datumI)
                 outputdata.append(datumO)
 
     else:
@@ -30,7 +26,7 @@ def makeData(operators):
         outputdata = np.array(outputdata)
         print(inputdata.shape)
         print(outputdata.shape)
-        # outputdata = outputdata.reshape(outputdata.size, 2)
+
         # shuffle input, output data
         tmp = np.hstack((inputdata, outputdata))
         np.random.shuffle(tmp)
@@ -39,16 +35,11 @@ def makeData(operators):
     return(inputdata, outputdata)
 
 if __name__ == '__main__':
-    # NN = nn.NeuralNetwork(layer_num=(2, 3, 1))
     NN = nn.NeuralNetwork(layer_num=(2, 10, 2))
 
     operators = ('+', '-')
     inputdata, outputdata = makeData(operators=operators)
-    # inputdata2, outputdata2 = makeData(operator='-')
-    # inputdata = inputdata1 + inputdata2
-    # outputdata = outputdata1 + outputdata2
 
-    # "+", "-" 1e-3 1000, "*" 3e-5 10000, "/" 1e-4 10000
     NN.setparams(mu=1e-3, MaxEpoch=1000)
     NN.train(inputdata=inputdata, outputdata=outputdata)
     # NN.save('weight.npz')
