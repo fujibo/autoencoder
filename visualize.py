aec6bb3fb4ae4142c246268f5b1ccf9ec53fc9c0
fujibo <kt.tsubo@gmail.com>
2016-12-15 08:45:48 +0900

    fix bug in visualizing #1

--- visualize.py
+++ visualize.py
@@ -16,5 +16,5 @@ if __name__ == '__main__':
         canvas = Image.new('L', (512, 512))
         for j in range(512):
             for k in range(512):
-                canvas.putpixel((j, k), datai[j, k])
+                canvas.putpixel((j, k), int(datai[j, k]))
         canvas.save('./DATA/image{:02d}.bmp'.format(i))
