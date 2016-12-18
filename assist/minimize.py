1e8a93830e99f7b94e6d6fb24000313f736afcd4
fujibo <kt.tsubo@gmail.com>
2016-12-18 12:28:09 +0900

    minimizing test for many variables

--- assist/minimize.py
+++ assist/minimize.py
@@ -1,9 +1,17 @@
-from scipy.optimize import minimize
-
-def func(x):
-    return x**4 + 5*x + 6
-def diff_func(x):
-    return 4 * x**3 + 5
+from scipy.optimize import minimize, rosen, rosen_der
+import numpy as np
+def func(z):
+    x =  z[0:2]
+    y = z[2:]
+    return x[0]**2 + x[1]**2 + y[0]**2 + (y[1]+1) ** 2
+def diff_func(z):
+    x =  z[0:2]
+    y = z[2:]
+    return np.array([2 * x[0], 2 * x[1], 2 * y[0], 2 * y[1] + 2])
 if __name__ == '__main__':
-    res = minimize(fun=func, x0=0, jac=diff_func, method='L-BFGS-B')
+    x0 = np.array([(1.0, 1.0), (1.0, 2.0)])
+    print(x0)
+    res = minimize(func, x0, jac=diff_func, method='L-BFGS-B')
+    print(res)
+    res = minimize(func, x0, method='L-BFGS-B')
     print(res)
